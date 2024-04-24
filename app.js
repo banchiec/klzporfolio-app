f8898f432d4faeb2f4efd54282a2a05ef3f46021
banchiec <icaro_19@hotmail.com>
2024-04-24 23:37:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require('dotenv').config()
+// ℹ️ Connects to the database
+require('./db')
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+var express = require('express')
+var app = express()
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require('./config')(app)
+// 👇 Start handling routes here
+var routes = require('./routes')
+app.use('/api', routes)
+//var authRoutes = require('./routes/auth.routes')
+//app.use('/auth', authRoutes)
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require('./error-handling')(app)
+module.exports = app
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const authRouter = require('./auth.routes/auth.routes')
+
+// router.get('/', (req, res, next) => {
+// 	res.json('All good in here')
+// })
+router.use('/auth', authRouter)
+
+module.exports = router
