d36046f7f4c05a377eaeb37b371829b6597648ff
unknown <daura38@gmail.com>
2024-06-06 19:52:58 +0200

    update multer

--- config/multer.js
+++ config/multer.js
@@ -1,8 +1,17 @@
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    const baseUrl = req.baseUrl;
+    const fileName = baseUrl.split("/")[2];
+    const uploadPath = `uploads/${fileName}`;
+
+    // Ensure the upload path exists
+    fs.mkdirSync(uploadPath, { recursive: true });
+
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
