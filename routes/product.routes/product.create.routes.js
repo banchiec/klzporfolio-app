e1a5849ff885b086b0a70e09b12fbf02a19ab2ba
unknown <daura38@gmail.com>
2024-06-06 20:52:38 +0200

    update upload

--- routes/product.routes/product.create.routes.js
+++ routes/product.routes/product.create.routes.js
@@ -1,8 +1,11 @@
-const express = require("express")
-const router = express.Router()
-const Product = require("../../models/product.model")
+const express = require("express");
+const router = express.Router();
+const Product = require("../../models/product.model");
+const upload = require("../../config/multer");
+const fs = require("fs");
+const path = require("path");
 
-router.post("/", async (req, res) => {
+router.post("/", upload.array("images", 10), async (req, res) => {
   const {
     name,
     description,
@@ -17,11 +20,15 @@ router.post("/", async (req, res) => {
     deliveryOptions,
     productType,
   } = req.body;
-  console.log(deliveryDays)
+
+  const imagesFiles = req.files;
 
   try {
-    const existsName = await Product.findOne({ name: name })
+    const existsName = await Product.findOne({ name: name });
     if (existsName === null) {
+      const images = [];
+      const uploadPath = `uploads/product/${req.baseUrl.split("/")[2]}`;
+
       const product = new Product({
         name,
         description,
@@ -39,7 +46,7 @@ router.post("/", async (req, res) => {
       await product.save();
       res.status(200).json(product);
     } else {
-      res.status(400).json({ error: "failed, name exist" })
+      res.status(400).json({ error: "failed, name exist" });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
