997e0770bf20e98e884e7a56aafeedfe2e20b6ca
banchiec <icaro_19@hotmail.com>
2024-06-06 20:08:41 +0200

    feature/product-create

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const authRouter = require("./auth.routes/auth.routes");
-const categoryRouter = require("./category.routes/category.routes");
-const productTypeRouter = require("./product.type.routes");
-const productRouter = require("./product.routes");
+const express = require('express')
+const router = express.Router()
+const authRouter = require('./auth.routes/auth.routes')
+const categoryRouter = require('./category.routes/category.routes')
+const productTypeRouter = require('./product.type.routes')
+const productRouter = require('./product.routes')
 
-router.use("/auth", authRouter);
-router.use("/category", categoryRouter);
-router.use("/product-type", productTypeRouter);
-router.use("/product", productRouter);
+router.use('/auth', authRouter)
+router.use('/categories', categoryRouter)
+router.use('/product-type', productTypeRouter)
+router.use('/products', productRouter)
 
-module.exports = router;
+module.exports = router
--- routes/product.routes/product.create.routes.js
+++ routes/product.routes/product.create.routes.js
@@ -1,49 +1,65 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const Product = require("../../models/product.model")
+const Product = require('../../models/product.model')
+const { uploadSingle } = require('../../middleware/upload')
+const upload = require('../../config/multer')
 
-router.post("/", async (req, res) => {
-  const {
-    name,
-    description,
-    deliveryDays,
-    colors,
-    category,
-    reviews,
-    deliveryFormat,
-    price,
-    sold,
-    images,
-    deliveryOptions,
-    productType,
-  } = req.body;
-  console.log(deliveryDays)
+router.post('/', upload.array('images', 12), async (req, res) => {
+	const {
+		name,
+		description,
+		deliveryDays,
+		colors,
+		category,
+		reviews,
+		deliveryFormat,
+		price,
+		sold,
+		images,
+		deliveryOptions,
+		productType,
+	} = req.body
 
-  try {
-    const existsName = await Product.findOne({ name: name })
-    if (existsName === null) {
-      const product = new Product({
-        name,
-        description,
-        deliveryDays,
-        colors,
-        category,
-        reviews,
-        deliveryFormat,
-        price,
-        sold,
-        images,
-        deliveryOptions,
-        productType,
-      });
-      await product.save();
-      res.status(200).json(product);
-    } else {
-      res.status(400).json({ error: "failed, name exist" })
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+	const imagePath = req.file ? req.file.path : null
 
-module.exports = router;
+	console.log(imagePath)
+	console.log(req.file)
+
+	const newImage = [
+		{
+			url: imagePath,
+			size: {
+				width: 210,
+				height: 210,
+			},
+		},
+	]
+
+	try {
+		const existsName = await Product.findOne({ name: name })
+		if (existsName === null) {
+			const product = new Product({
+				name,
+				description,
+				deliveryDays,
+				colors,
+				category,
+				reviews,
+				deliveryFormat,
+				price,
+				sold,
+				images: newImage,
+				deliveryOptions,
+				productType,
+			})
+			await product.save()
+			res.status(200).json(product)
+		} else {
+			res.status(400).json({ error: 'failed, name exist' })
+		}
+	} catch (error) {
+		res.status(500).json({ error: error.message })
+	}
+})
+
+module.exports = router
