247ef4242b741cab8f2753a60dd57b1abf915295
banchiec <icaro_19@hotmail.com>
2024-06-09 20:37:13 +0200

    feature/product-create add product-type upload route

--- routes/uploads.routes/single.upload.routes.js
+++ routes/uploads.routes/single.upload.routes.js
@@ -4,8 +4,13 @@ const path = require('path')
 
 router.get('/categories/:image', async (req, res) => {
 	const { image } = req.params
+	console.log(image)
 	res.sendFile(path.join(__dirname, `../../uploads/categories/${image}`))
-	//res.sendFile(__dirname + `../../../uploads/categories/${image}`)
+})
+
+router.get('/product-type/:image', async (req, res) => {
+	const { image } = req.params
+	res.sendFile(path.join(__dirname, `../../uploads/product-type/${image}`))
 })
 
 module.exports = router
